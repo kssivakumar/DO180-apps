412371f11f71ca651d1888e70bdc5679234dd16b
Student User <student@workstation.lab.example.com>
2020-08-20 13:55:44 +0000

    Changed index

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,3 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A Change is coming!\n";
-print "A Change is in the air!\n";
-?>
+    print "Hello, World! php version is " . PHP_VERSION . "\n";
+    ?>
